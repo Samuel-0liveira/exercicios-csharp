aaa7f74c3b49cd01cf28a98b3e037e3f42c206bf
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-02 13:33:55 -0300

    Add files via upload

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Faça um programa que:
+- Leia a cotação do dólar
+- Leia um valor em dólares
+- Converta esse valor para Real
+- Mostre o resultado
+*/
+
+using System;
+using System.Globalization;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Por favor, digite a cotação atual do dólar: ");
+            decimal cotacaoDolar = decimal.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
+
+            Console.Write("Agora, digite digite o valor em dólares para conversão: ");
+            decimal valorEmDolar = decimal.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
+
+            ConversaoDolarParaReal(cotacaoDolar, valorEmDolar);
+        }
+
+        public static void ConversaoDolarParaReal(decimal cotacao, decimal valorDolar)
+        {
+            decimal valorEmReal = cotacao * valorDolar;
+
+            Console.WriteLine($"O valor em dólar digitado convertido para real é igual a: {valorEmReal.ToString("C", CultureInfo.CreateSpecificCulture("pt-Br"))}");
+            Console.ReadKey();
+        }
+    }
+}
