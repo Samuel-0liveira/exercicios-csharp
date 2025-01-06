0e60e1d2f61ac8bcefce8ba4e4213afe6543599f
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-06 16:29:56 -0300

    Add files via upload

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão de
+temperatura a ser utilizada é F = (9 * C + 160) / 5, em que a variável F representa é a temperatura em graus
+Fahrenheit e a variável C representa é a temperatura em graus Celsius.
+*/
+
+using System;
+
+namespace Exercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversaoDeCelsiusParaFahrenheit();
+            Console.ReadKey();
+        }
+
+        static void ConversaoDeCelsiusParaFahrenheit()
+        {
+            Console.Clear();
+
+            Console.Write("Por favor, digite a temperatura em Celsius para converter em Fahrenheit: ");
+            var celsius = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
+
+            float fahrenheit = (9 * celsius + 160) / 5;
+
+            Console.WriteLine($"O valor {celsius}°C convertido para Fahrenheit é igual a: {fahrenheit:n1}°F");
+        }
+    }
+}
