ea299286682230e8e183b5b24853e3189ced4407
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-10 17:48:59 -0300

    Add files via upload

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Escreva um programa que leia um número inteiro (variável CODIGO). Verificar se o código é igual a 1, igual a 2 ou
+igual a 3. Caso não seja, apresentar a mensagem "Código inválido".Ao ser verificado o código e constatado que é
+um valor válido, o programa deve verificar cada código em separado para determinar seu valor por extenso,ou
+seja,apresentar a mensagem "um","dois"ou "três".(Utilizar ocomando Switch).
+*/
+
+using System;
+
+namespace Exercicio18;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.Write("Por favor, digite um número entre 1 e 3: ");
+        int codigo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");
+
+        switch(codigo)
+        {
+            case 1:
+                Console.WriteLine("Um");
+                break;
+            case 2:
+                Console.WriteLine("Dois");
+                break;
+            case 3:
+                Console.WriteLine("Três");
+                break;
+            default:
+                Console.WriteLine("Código Inválido");
+                break;
+        }
+
+        Console.ReadKey();
+    }
+}
