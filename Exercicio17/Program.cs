c747428c5e9284cb1592505f8bac1692adbb3d06
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-10 17:48:26 -0300

    Add files via upload

---
+++ Exercicio17/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Escreva um programa que leia um número inteiro. Verificar por meio de condição se o valor fornecido está na faixa
+entre O (zero) e 9 (nove). Caso o valor fornecido esteja dentro da faixa, apresentar a mensagem "valor válido".
+Caso contrário, apresentar a mensagem "valor inválido".
+*/
+
+using System;
+
+namespace Exercicio17;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.Write("Olá, por favor, digite um número entre 0 e 9: ");
+        int valor = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");
+
+        VerificarIntervalo(valor);
+        Console.ReadKey();
+    }
+
+    static void VerificarIntervalo(int numero)
+    {
+        if (numero >= 0 && numero <= 9)
+        {
+            Console.WriteLine("Valor válido!");
+        } else {
+            Console.WriteLine("Valor inválido!");
+        }
+    }
+}
