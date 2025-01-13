3e83ef7c7b8e0392f53ef96beb0f7a3a06419edf
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-13 16:49:32 -0300

    Add files via upload

---
+++ Exercicio25/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Tendo como dados de entrada a altura e o sexo de uma pessoa, faça um programa que calcule seu peso ideal,
+utilizando as seguintes fórmulas:(h=altura)
+- Para homens:(72.7*h) - 58
+- Para mulheres: (62.1 *h) - 44.7
+*/
+
+using System;
+
+namespace Exercicio25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Olá, digite a altura da pessoa: ");
+            float altura = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
+
+            Console.Write("Agora, digite o sexo da pessoa [M / F]: ");
+            char sexo = char.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
+
+            CalcularPesoIdeal(altura, sexo);
+            Console.ReadKey();
+        }
+
+        static void CalcularPesoIdeal(float altura, char sexo)
+        {
+            double pesoIdeal = 0;
+
+            switch(sexo)
+            {
+                case 'm':
+                case 'M':
+                    pesoIdeal = (72.7 * altura) - 58;
+                    break;
+                case 'f':
+                case 'F':
+                    pesoIdeal = (62.1 * altura) - 44.7;
+                    break;
+                default:
+                    Console.WriteLine("Opção inexistente digitada!");
+                    break;
+            }
+
+            Console.WriteLine($"O peso ideal para a pessoa em questão é: {pesoIdeal:n1}Kg");
+        }
+    }
+}
