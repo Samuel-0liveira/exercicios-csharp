9a02003864cce101cc02d5c33908960e14a5a06b
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-14 13:46:02 -0300

    Add files via upload

---
+++ Exercicio28/Program.cs
@@ -0,0 +1,22 @@
+﻿//Faça um programa que gera e escreve os números ímpares dos números lidos entre 100 e 200.
+
+namespace Exercicio28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Esses são os números ímpares que estão entre 100 e 200: ");
+
+            for (int contador = 100; contador <= 200; contador++) {
+                if (contador % 2 != 0) {
+                    Console.WriteLine(contador);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
