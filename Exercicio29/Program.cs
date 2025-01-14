cfbcdf75d4b5e4eff07550342d94c6e6a8ca99df
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-14 13:46:53 -0300

    Add files via upload

---
+++ Exercicio29/Program.cs
@@ -0,0 +1,16 @@
+﻿//Faça um programa que exiba os números de 1 até 2000.
+
+namespace Exercicio29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int contador = 1; contador <= 2000; contador++) {
+                Console.Write(contador+", ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
