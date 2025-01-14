8981d6efe78fdf3c6a2ccd84f6baeb148807a944
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-14 13:47:31 -0300

    Add files via upload

---
+++ Exercicio30/Program.cs
@@ -0,0 +1,30 @@
+﻿//Faça um programa para imprimir uma tabuada
+
+using System.Net;
+
+namespace Exercicio30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Deseja ver a tabuada de qual número?: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+                Tabuada(numero);
+                Console.ReadKey();
+        }
+
+        static void Tabuada(int numero)
+        {
+            Console.WriteLine($"A tabuada do número: {numero}");
+
+            for (int index = 1; index <= 10; index++) {
+                Console.Write($"{index} x {numero} = {index * numero}\n");
+            }
+        }
+    }
+}
