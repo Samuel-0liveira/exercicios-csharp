e087a1a50cf84cde88311e9d12a0de5c916552f5
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-16 16:12:18 -0300

    Add files via upload

---
+++ Exercicio35/Program.cs
@@ -0,0 +1,22 @@
+﻿//Faça um programa que conte de 1 até 100 e a cada múltiplo de 10 exiba uma mensagem: "Múltiplo de 10".
+
+namespace Exercicio35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int contador = 1; contador <= 100; contador++)
+            {
+                if (contador % 10 == 0) {
+                    Console.WriteLine(contador + " - Múltiplo de 10!");
+                } else {
+                    
+                Console.WriteLine(contador);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
