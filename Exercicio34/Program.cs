7a1e05e5271ea8f0c8dc70091941baa3e858ad15
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-16 16:10:34 -0300

    Add files via upload

---
+++ Exercicio34/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Escreva um programa que leia um conjunto de números positivos e exiba o menor e o maior. Suporemos que o
+número de elementos deste conjunto não é conhecido, e que um número negativo será utilizado para sinalizar o
+fim dos dados.
+*/
+
+namespace Exercicio34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior, menor, numero;
+            menor = 100;
+            maior = numero = 0;
+
+            while (numero >= 0) {
+                Console.Clear();
+
+                Console.Write("Por favor, digite um número (caso queira parar, digite um valor negativo): ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+
+                if (numero < 0) {
+                    Console.WriteLine("Encerrando...");
+                } else {
+                    if (numero >= maior) {
+                        maior = numero;
+                    }
+                    
+                    if (numero <= menor) {
+                        menor = numero;
+                    }
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"O maior número digitado foi: {maior}\nO menor digitado foi: {menor}");
+            Console.ReadKey();
+        }
+    }
+}
