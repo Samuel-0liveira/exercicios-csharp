4b2cae70140d32108a7c4c3fa06cad239f6e0525
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-18 19:24:47 -0300

    Add files via upload

---
+++ Exercicio45/Program.cs
@@ -0,0 +1,33 @@
+﻿//Dada uma seqüência de n números (vetor de inteiros), imprimi-la na ordem inversa que foi realizada a leitura.
+
+namespace Exercicio45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Digite quantos números terão na sequência: ");
+            int quantidade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+            int[] valores = new int[quantidade];
+
+            for (int contador = 0; contador < quantidade; contador++) {
+                Console.Write("Digite o " + (contador + 1) +"º número: ");
+                valores[contador] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Os valores lidos, em ordem inversa ficariam dessa forma: ");
+            Array.Reverse(valores);
+
+            for (int contador = 0; contador < quantidade; contador++) {
+                Console.Write(valores[contador] + ", ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
