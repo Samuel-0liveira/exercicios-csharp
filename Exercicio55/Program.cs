6108483ee09d3eea30b41e33f1daf0c7c4f66763
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-23 12:52:39 -0300

    Add files via upload

---
+++ Exercicio55/Program.cs
@@ -0,0 +1,42 @@
+﻿//Fazer um programa que leia uma frase de até 50 caracteres e imprima a frase sem os espaços em branco.
+//Imprimir também a quantidade de espaços em branco da frase
+
+namespace Exercicio55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            string frase = "";
+            int espacos = 0;
+
+            do {
+                Console.Clear();
+                
+                Console.Write("Por favor, digite uma frase(máximo 50 caracteres): ");
+                frase = Console.ReadLine()!;
+                Console.WriteLine("");
+            } while (frase.Length > 50);
+
+            Console.Clear();
+
+            char[] fraseSeparada = frase.ToCharArray();
+
+            Console.WriteLine("\nA frase sem espaços em branco ficaria da seguinte forma: \n");
+
+            for (int contador = 0; contador < fraseSeparada.Length; contador++) {
+                if (fraseSeparada[contador].Equals(' ')) {
+                    espacos++;
+                } else {
+                    Console.Write(fraseSeparada[contador]);
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"\nA frase continha {espacos} espaços em branco.");
+            Console.ReadKey();
+        }
+    }
+}
