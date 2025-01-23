8bfa1b993933005ccc26223ec157987f9e5c1eb2
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-23 12:53:46 -0300

    Add files via upload

---
+++ Exercicio57/Program.cs
@@ -0,0 +1,49 @@
+﻿//Fazer um programa que leia uma frase e imprima somente as vogais
+
+namespace Exercicio57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Digite uma frase: ");
+            string frase = Console.ReadLine()!;
+            Console.WriteLine("");
+
+            char[] fraseSeparada = frase.ToCharArray();
+
+            Console.WriteLine("A frase somente com as vogais ficaria da seguinte forma: \n");
+
+
+            for (int contador = 0; contador < fraseSeparada.Length; contador++) {
+                switch (fraseSeparada[contador]) {
+                    case 'a':
+                    case 'A':
+                        Console.Write(fraseSeparada[contador]);
+                        break;
+                    case 'e':
+                    case 'E':
+                        Console.Write(fraseSeparada[contador]);
+                        break;
+                    case 'i':
+                    case 'I':
+                        Console.Write(fraseSeparada[contador]);
+                        break;
+                    case 'o':
+                    case 'O':
+                        Console.Write(fraseSeparada[contador]);
+                        break;
+                    case 'u':
+                    case 'U':
+                        Console.Write(fraseSeparada[contador]);
+                        break;
+                }
+            }
+            
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
