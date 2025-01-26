2a677d9f86a0fd5591f713d63b1e41526fe07997
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-26 13:43:15 -0300

    Add files via upload

---
+++ Exercicio63/Program.cs
@@ -0,0 +1,35 @@
+﻿//Escreva um programa que verifique se um número é par ou ímpar através de um método chamado VERIFICA . O
+//método deverá receber um número inteiro (n) e deverá retornar a mensagem "PAR" ou "ÍMPAR".
+
+namespace Exercicio63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Olá, digite um número inteiro: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+            string retorno = Verificar(numero);
+
+            Console.WriteLine($"{retorno}");
+            Console.ReadKey();
+        }
+
+        static string Verificar(int n) 
+        {
+            string resultado = "";
+
+            if (n % 2 == 0) {
+                resultado = "PAR";
+            } else {
+                resultado = "ÍMPAR";
+            }
+
+            return resultado;
+        }
+    }
+}
