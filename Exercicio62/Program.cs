d76245cc789a74a4bce378aa95a418f13107773d
Samuel Gonçalves de Oliveira <87822392+Samuel-0liveira@users.noreply.github.com>
2025-01-26 13:41:31 -0300

    Add files via upload

---
+++ Exercicio62/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Escreva um programa que calcule e retorne o valor da hipotenusa através do método HIPOTENUSA. O método 
+recebe o valor da base e da altura de um triângulo.
+Fórmulas: hipotenusa2 = base2 + altura2
+           base x altura
+área = ----------------------
+                2
+*/
+
+namespace Exercicio62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Por favor, digite a base do triângulo: ");
+            float baseTriangulo = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
+
+            Console.Write("Agora, digite a altura do triângulo: ");
+            float alturaTriangulo = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
+
+            double area = Hipotenusa(baseTriangulo, alturaTriangulo);
+
+            Console.WriteLine($"O valor da hipotenusa é: {area:n1}");
+            Console.ReadKey();
+        }
+
+        static double Hipotenusa(float baseT, float alturaT)
+        {
+            double hipotenusa = 0;
+
+            baseT *= baseT;
+            alturaT *= alturaT;
+
+            hipotenusa = baseT + alturaT;
+
+            hipotenusa = Math.Sqrt(hipotenusa);
+
+            return hipotenusa;
+        }
+    }
+}
